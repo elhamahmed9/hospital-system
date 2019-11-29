baca1214f7dfabed711ee435d8ad18099eacdfc8
elham ahmed <elhamahmed584@gmail.com>
2019-11-29 20:44:35 +0200

    Add files via upload

--- SQL File/hms.sql
+++ SQL File/hms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2019 at 03:09 PM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
+-- Generation Time: Nov 29, 2019 at 07:42 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,7 +40,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
-(1, 'admin', 'Test@12345', '28-12-2016 11:42:05 AM');
+(1, 'admin', 'Test@12345', '29-11-2019 1:42:05 AM');
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ CREATE TABLE `appointment` (
   `consultancyFees` int(11) NOT NULL,
   `appointmentDate` varchar(255) NOT NULL,
   `appointmentTime` varchar(255) NOT NULL,
-  `postingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `postingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `userStatus` int(11) NOT NULL,
   `doctorStatus` int(11) NOT NULL,
   `updationDate` varchar(255) NOT NULL
@@ -67,9 +67,9 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
-(1, 'Dentist', 1, 1, 500, '2016-12-31', '09:25', '2017-01-01 00:29:02', 1, 0, ''),
-(2, 'Homeopath', 4, 5, 700, '2017-01-11', '14:10', '2017-01-07 08:02:58', 0, 1, ''),
-(3, 'Ayurveda', 8, 6, 600, '2019-06-29', '9:15 AM', '2019-06-23 18:31:28', 1, 1, '');
+(1, 'Dentist', 1, 1, 500, '2019-11-29', '09:25', '2019-11-29 00:29:02', 1, 0, ''),
+(2, 'Homeopath', 4, 5, 700, '2019-11-29', '14:10', '2019-11-29 08:02:58', 0, 1, ''),
+(3, 'Ayurveda', 8, 6, 600, '2019-11-29', '9:15 AM', '2019-11-29 18:31:28', 1, 1, '');
 
 -- --------------------------------------------------------
 
@@ -81,13 +81,13 @@ CREATE TABLE `doctors` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL,
   `doctorName` varchar(255) DEFAULT NULL,
-  `address` longtext DEFAULT NULL,
+  `address` longtext,
   `docFees` varchar(255) DEFAULT NULL,
   `contactno` bigint(11) DEFAULT NULL,
   `docEmail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -95,14 +95,9 @@ CREATE TABLE `doctors` (
 --
 
 INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
-(1, 'Dentist', 'Anuj', 'New Delhi', '500', 8285703354, 'anuj.lpu1@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-29 06:25:37', '2019-06-30 12:11:05'),
-(2, 'Homeopath', 'Sarita Pandey', 'Varanasi', '600', 2147483647, 'sarita@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-29 06:51:51', '0000-00-00 00:00:00'),
-(3, 'General Physician', 'Nitesh Kumar', 'Ghaziabad', '1200', 8523699999, 'nitesh@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:43:35', '0000-00-00 00:00:00'),
-(4, 'Homeopath', 'Vijay Verma', 'New Delhi', '700', 25668888, 'vijay@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:45:09', '0000-00-00 00:00:00'),
-(5, 'Ayurveda', 'Sanjeev', 'Gurugram', '8050', 442166644646, 'sanjeev@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:47:07', '0000-00-00 00:00:00'),
-(6, 'General Physician', 'Amrita', 'New Delhi India', '2500', 45497964, 'amrita@test.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:52:50', '0000-00-00 00:00:00'),
-(7, 'Demo test', 'abc ', 'New Delhi India', '200', 852888888, 'test@demo.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 08:08:58', '2019-06-23 18:17:25'),
-(8, 'Ayurveda', 'Test Doctor', 'Xyz Abc New Delhi', '600', 1234567890, 'test@test.com', '202cb962ac59075b964b07152d234b70', '2019-06-23 17:57:43', '2019-06-23 18:06:06');
+(9, 'Gynecologist/Obstetrician', 'ahmed hassan', 'dokki/cairo', '200', 34563213, 'test@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2019-11-28 23:40:47', NULL),
+(10, 'Homeopath', 'noha ali', 'maadi/cairo', '230', 3246765867, 'test2@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2019-11-28 23:41:43', NULL),
+(11, 'Dentist', 'elham fathy', 'nasr city', '300', 882837093, 'test3@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2019-11-28 23:42:35', NULL);
 
 -- --------------------------------------------------------
 
@@ -115,7 +110,7 @@ CREATE TABLE `doctorslog` (
   `uid` int(11) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `userip` binary(16) DEFAULT NULL,
-  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `loginTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `logout` varchar(255) DEFAULT NULL,
   `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -125,16 +120,18 @@ CREATE TABLE `doctorslog` (
 --
 
 INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(1, 2, 'sarita@gmail.com', 0x30000000000000000000000000000000, '2017-01-06 05:53:31', '', 1),
-(2, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 06:36:07', '', 0),
-(3, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:36:37', '06/01/2017 07:36:45', 1),
-(4, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:41:33', '12:11:46', 1),
-(5, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:55:16', '06-01-2017 12:27:47 PM', 1),
-(6, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 07:07:12', '', 0),
-(7, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:04:42', '', 0),
-(8, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:04:55', '', 0),
-(9, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:05:54', '07-01-2017 01:36:28 PM', 1),
-(10, 7, 'test@demo.com', 0x3a3a3100000000000000000000000000, '2019-06-23 18:15:31', '23-06-2019 11:47:36 PM', 1);
+(1, 2, 'sarita@gmail.com', 0x30000000000000000000000000000000, '2019-11-29 05:53:31', '', 1),
+(2, 0, 'admin', 0x3a3a3100000000000000000000000000, '2019-11-29 06:36:07', '', 0),
+(3, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 06:36:37', '2019-11-29 07:36:45', 1),
+(4, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 06:41:33', '12:11:46', 1),
+(5, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 06:55:16', '2019-11-29 12:27:47 PM', 1),
+(6, 0, 'admin', 0x3a3a3100000000000000000000000000, '2019-11-29 07:07:12', '', 0),
+(7, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2019-11-29 08:04:42', '', 0),
+(8, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2019-11-29 08:04:55', '', 0),
+(9, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 08:05:54', '2019-11-29 01:36:28 PM', 1),
+(10, 7, 'test@demo.com', 0x3a3a3100000000000000000000000000, '2019-06-23 18:15:31', '23-06-2019 11:47:36 PM', 1),
+(11, NULL, 'admin', 0x3a3a3100000000000000000000000000, '2019-11-29 15:26:20', NULL, 0),
+(12, NULL, 'info@w3gang.com ', 0x3a3a3100000000000000000000000000, '2019-11-29 15:27:13', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -145,8 +142,8 @@ INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logou
 CREATE TABLE `doctorspecilization` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL,
-  `creationDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -154,16 +151,14 @@ CREATE TABLE `doctorspecilization` (
 --
 
 INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
-(1, 'Gynecologist/Obstetrician', '2016-12-28 06:37:25', '0000-00-00 00:00:00'),
-(2, 'General Physician', '2016-12-28 06:38:12', '0000-00-00 00:00:00'),
-(3, 'Dermatologist', '2016-12-28 06:38:48', '0000-00-00 00:00:00'),
-(4, 'Homeopath', '2016-12-28 06:39:26', '0000-00-00 00:00:00'),
-(5, 'Ayurveda', '2016-12-28 06:39:51', '0000-00-00 00:00:00'),
-(6, 'Dentist', '2016-12-28 06:40:08', '0000-00-00 00:00:00'),
-(7, 'Ear-Nose-Throat (Ent) Specialist', '2016-12-28 06:41:18', '0000-00-00 00:00:00'),
-(9, 'Demo test', '2016-12-28 07:37:39', '0000-00-00 00:00:00'),
-(10, 'Bones Specialist demo', '2017-01-07 08:07:53', '0000-00-00 00:00:00'),
-(11, 'Test', '2019-06-23 17:51:06', '2019-06-23 17:55:06');
+(12, 'Gynecologist/Obstetrician', '2019-11-28 23:37:41', NULL),
+(13, 'General Physician', '2019-11-28 23:38:00', NULL),
+(14, 'Dermatologist', '2019-11-28 23:38:12', NULL),
+(15, 'Homeopath', '2019-11-28 23:38:23', NULL),
+(16, 'Ayurveda', '2019-11-28 23:38:32', NULL),
+(17, 'Dentist', '2019-11-28 23:38:42', NULL),
+(18, 'Ear-Nose-Throat', '2019-11-28 23:38:51', NULL),
+(19, 'Bones Specialist demo', '2019-11-28 23:39:05', NULL);
 
 -- --------------------------------------------------------
 
@@ -176,10 +171,10 @@ CREATE TABLE `tblcontactus` (
   `fullname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `contactno` bigint(12) DEFAULT NULL,
-  `message` mediumtext DEFAULT NULL,
-  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
-  `AdminRemark` mediumtext DEFAULT NULL,
-  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `message` mediumtext,
+  `PostingDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `AdminRemark` mediumtext,
+  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `IsRead` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -188,8 +183,10 @@ CREATE TABLE `tblcontactus` (
 --
 
 INSERT INTO `tblcontactus` (`id`, `fullname`, `email`, `contactno`, `message`, `PostingDate`, `AdminRemark`, `LastupdationDate`, `IsRead`) VALUES
-(1, 'test user', 'test@gmail.com', 2523523522523523, ' This is sample text for the test.', '2019-06-29 19:03:08', 'Test Admin Remark', '2019-06-30 12:55:23', 1),
-(2, 'Anuj kumar', 'phpgurukulofficial@gmail.com', 1111111111111111, ' This is sample text for testing.  This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing.', '2019-06-30 13:06:50', NULL, NULL, NULL);
+(1, 'test user', 'test@gmail.com', 2523523522523523, ' This is sample text for the test.', '2019-11-29 19:03:08', 'Test Admin Remark', '2019-11-29 12:55:23', 1),
+(2, 'Anuj kumar', 'phpgurukulofficial@gmail.com', 1111111111111111, ' This is sample text for testing.  This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing. This is sample text for testing.', '2019-06-30 13:06:50', NULL, NULL, NULL),
+(3, 'hghghg', 'ahel@yahoo.com', 9322, ' iojfdkslvmf', '2019-10-04 15:25:32', NULL, NULL, NULL),
+(4, 'Elham Ahmed Shahen', 'ahelham185@yahoo.com', 1278659634, 'mzXVNjZN <Xo', '2019-11-28 23:59:43', 'thanks 4 ur msg', '2019-11-29 00:02:59', 1);
 
 -- --------------------------------------------------------
 
@@ -202,7 +199,7 @@ CREATE TABLE `userlog` (
   `uid` int(11) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `userip` binary(16) DEFAULT NULL,
-  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `loginTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `logout` varchar(255) DEFAULT NULL,
   `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -212,16 +209,17 @@ CREATE TABLE `userlog` (
 --
 
 INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(6, 1, '', 0x3a3a3100000000000000000000000000, '2017-01-06 07:02:28', '', 1),
-(7, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:04:28', '', 1),
-(8, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 07:07:41', '06-01-2017 12:38:09 PM', 0),
-(9, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:08:01', '', 1),
-(10, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:10:09', '06-01-2017 12:41:43 PM', 1),
-(11, 2, 'test@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 07:57:18', '07-01-2017 01:27:34 PM', 1),
-(12, 0, 'asdad', 0x3a3a3100000000000000000000000000, '2017-01-07 07:57:44', '', 0),
-(13, 0, 'xyz@test.com', 0x3a3a3100000000000000000000000000, '2017-01-07 07:59:43', '', 0),
-(14, 5, 'amit12@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:00:44', '07-01-2017 01:34:19 PM', 1),
-(15, 6, 'tetuser@gmail.com', 0x3a3a3100000000000000000000000000, '2019-06-23 18:30:16', '24-06-2019 12:10:17 AM', 1);
+(6, 1, '', 0x3a3a3100000000000000000000000000, '2019-11-29 07:02:28', '', 1),
+(7, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2019-11-29 07:04:28', '', 1),
+(8, 0, 'admin', 0x3a3a3100000000000000000000000000, '2019-11-29 07:07:41', '2019-11-29 12:38:09 PM', 0),
+(9, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2019-11-29 07:08:01', '', 1),
+(10, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2019-11-29 07:10:09', '2019-11-29 12:41:43 PM', 1),
+(11, 2, 'test@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 07:57:18', '2019-11-29 01:27:34 PM', 1),
+(12, 0, 'asdad', 0x3a3a3100000000000000000000000000, '2019-11-29 07:57:44', '', 0),
+(13, 0, 'xyz@test.com', 0x3a3a3100000000000000000000000000, '2019-11-29 07:59:43', '', 0),
+(14, 5, 'amit12@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 08:00:44', '2019-11-29 01:34:19 PM', 1),
+(15, 6, 'tetuser@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-29 18:30:16', '2019-11-29 12:10:17 AM', 1),
+(16, NULL, 'test@gmail.com', 0x3a3a3100000000000000000000000000, '2019-11-28 17:06:02', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -232,13 +230,13 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fullName` varchar(255) DEFAULT NULL,
-  `address` longtext DEFAULT NULL,
+  `address` longtext,
   `city` varchar(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `regDate` timestamp NULL DEFAULT current_timestamp(),
-  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `regDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -246,13 +244,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
-(1, 'Anuj kumar', 'Test address', 'Aligarh', 'Males', 'info@w3gang.com', '5c428d8875d2948607f3e3fe134d71b4', '2016-12-26 07:03:09', '2019-06-30 11:59:05'),
-(2, 'Sarita pandey', 'New Delhi India', 'Delhi', 'female', 'test@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-30 05:34:39', '0000-00-00 00:00:00'),
-(3, 'Amit', 'New Delhi', 'New delhi', 'male', 'amit@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 06:36:53', '0000-00-00 00:00:00'),
-(4, 'Rahul Singh', 'New Delhi', 'New delhi', 'male', 'rahul@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:41:14', '0000-00-00 00:00:00'),
-(5, 'Amit kumar', 'New Delhi India', 'Delhi', 'male', 'amit12@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 08:00:26', '0000-00-00 00:00:00'),
-(6, 'Test user', 'New Delhi', 'Delhi', 'male', 'tetuser@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-06-23 18:24:53', '2019-06-23 18:36:09'),
-(7, 'cdsf', 'fsdfsd', 'fdsf', 'male', 'fdsfsd2312@dsfsd.com', 'c20ad4d76fe97759aa27a0c99bff6710', '2019-06-23 18:29:36', NULL);
+(7, 'cdsf', 'fsdfsd', 'fdsf', 'male', 'fdsfsd2312@dsfsd.com', 'c20ad4d76fe97759aa27a0c99bff6710', '2019-11-29 18:29:36', NULL),
+(8, 'elham ahmed ibrahim shahen', 'Ø§Ù„Ø¯Ù‚Ù‡Ù„ÙŠØ© -Ø§Ù„Ù…Ù†Ø²Ù„Ø©', 'Ø§Ù„Ø¯Ù‚Ù‡Ù„ÙŠØ©', 'female', 'ahelham185@yahoo.com', 'e10adc3949ba59abbe56e057f20f883e', '2019-11-29 15:32:54', NULL);
 
 --
 -- Indexes for dumped tables
@@ -326,37 +319,37 @@ ALTER TABLE `appointment`
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `doctorslog`
 --
 ALTER TABLE `doctorslog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `doctorspecilization`
 --
 ALTER TABLE `doctorspecilization`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tblcontactus`
 --
 ALTER TABLE `tblcontactus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `userlog`
 --
 ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
